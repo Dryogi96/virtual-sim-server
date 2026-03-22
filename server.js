10fd1c6271d2dfe8dbbdb020dcd3b0f93efefeba
Dryogi96 <ysatkar7@gmail.com>
2026-03-22 18:56:33 +0530

    fixed path for railway

--- server.js
+++ server.js
@@ -6,49 +6,38 @@ const { Server } = require("socket.io")
 
 const app = express()
 
-/* -----------------------------
-   Middleware
-------------------------------*/
+/* ----------------------------- */
 app.use(cors())
 app.use(express.json())
 
-/* -----------------------------
-   Database (FIXED PATH)
-------------------------------*/
-const connectDB = require("../server/config/database")
+/* ----------------------------- */
+/* DATABASE */
+const connectDB = require("./server/config/database")
 connectDB()
 
-/* -----------------------------
-   Static uploads
-------------------------------*/
-app.use("/uploads", express.static(path.join(__dirname, "../server/uploads")))
+/* ----------------------------- */
+/* STATIC */
+app.use("/uploads", express.static(path.join(__dirname, "server/uploads")))
 
-/* -----------------------------
-   Routes (FIXED PATH)
-------------------------------*/
-const authRoutes = require("../server/routes/auth")
-const groupRoutes = require("../server/routes/groups")
-const mediaRoutes = require("../server/routes/media")
+/* ----------------------------- */
+/* ROUTES */
+const authRoutes = require("./server/routes/auth")
+const groupRoutes = require("./server/routes/groups")
+const mediaRoutes = require("./server/routes/media")
 
 app.use("/auth", authRoutes)
 app.use("/groups", groupRoutes)
 app.use("/media", mediaRoutes)
 
-/* -----------------------------
-   Root
-------------------------------*/
+/* ----------------------------- */
 app.get("/", (req, res) => {
     res.send("Virtual SIM Server Running 🚀")
 })
 
-/* -----------------------------
-   Server
-------------------------------*/
+/* ----------------------------- */
 const server = http.createServer(app)
 
-/* -----------------------------
-   Socket.IO
-------------------------------*/
+/* ----------------------------- */
 const io = new Server(server, {
     cors: {
         origin: "*",
@@ -56,15 +45,11 @@ const io = new Server(server, {
     }
 })
 
-/* -----------------------------
-   Chat Socket (FIXED PATH)
-------------------------------*/
-const chatSocket = require("../server/sockets/chatSocket")
+/* ----------------------------- */
+const chatSocket = require("./server/sockets/chatSocket")
 chatSocket(io)
 
-/* -----------------------------
-   Start
-------------------------------*/
+/* ----------------------------- */
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
